8cde9cf421c3425ec286b3aa4f1999f78f6b3f18
ysskrishna <sivasaikrishnassk@gmail.com>
2024-11-30 21:43:22 +0530

    Siva | add rabbitmq configuration

--- webserver/webserver/src/core/config.py
+++ webserver/webserver/src/core/config.py
@@ -11,4 +11,10 @@ class Config(object):
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
     
+    RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
+    RABBITMQ_PORT = os.getenv('RABBITMQ_PORT')
+    RABBITMQ_USER = os.getenv('RABBITMQ_USER')
+    RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+    RABBITMQ_VHOST = os.getenv('RABBITMQ_VHOST')
+    
     DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
