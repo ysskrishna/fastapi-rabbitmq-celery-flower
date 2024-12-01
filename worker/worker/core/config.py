8a7001ed015c5ffbf675f4a15187b6e4bba6b119
ysskrishna <sivasaikrishnassk@gmail.com>
2024-12-01 15:19:26 +0530

    Siva | worker | add rabbitmq configuration

---
+++ worker/worker/core/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+    RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+    RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+    RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
+    
+    CELERY_BROKER_URL = f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}"
