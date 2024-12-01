a9159f56f97fe93e60f3f660dfe7f994a199bdc0
ysskrishna <sivasaikrishnassk@gmail.com>
2024-12-01 18:11:25 +0530

    Siva | dockerize celery worker

--- worker/worker/core/config.py
+++ worker/worker/core/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
-    RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+    RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT"))
     RABBITMQ_USER = os.getenv("RABBITMQ_USER")
     RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
     
